727bc90b3cefdd49d4d3b209d712d269e8f9a505
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2024-06-03 14:23:10 -0400

    Initial Commit

---
+++ MyHark/MyHarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHarkApp.swift
+//  MyHark
+//
+//  Created by Cari van der Merwe on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct MyHarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
