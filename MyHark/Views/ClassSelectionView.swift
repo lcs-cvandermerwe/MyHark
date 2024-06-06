1e2fa5c5638c19c58a6ca9b9c741bb340c2c101b
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2024-06-06 09:27:15 -0400

    Finished the Class Selection View

--- MyHark/Views/ClassSelectionView.swift
+++ MyHark/Views/ClassSelectionView.swift
@@ -15,26 +15,72 @@ struct ClassSelectionView: View {
                     .ignoresSafeArea()
                 VStack {
                     Image(systemName: "graduationcap.fill")
+                        .foregroundColor(.brightBlue)
                         .font(.custom("a", size: 60))
                     
                     Text("Which Grade are you in?")
                         .foregroundStyle(Color.white)
-                        .font(.custom("EuphemiaUCAS-Bold", size: 20))
+                        .font(.custom("EuphemiaUCAS-Bold", size: 30.0))
+                    HStack{
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.brightBlue)
+                            Text("9")
+                                .foregroundStyle(Color.white)
+                                .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
+                        }
+                        .frame(height: 100)
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.brightBlue)
+                            Text("10")
+                                .foregroundStyle(Color.white)
+                                .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
+                        }
+                        .frame(height: 100)
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.brightBlue)
+                            Text("11")
+                                .foregroundStyle(Color.white)
+                                .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
+                        }
+                        .frame(height: 100)
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.brightBlue)
+                            Text("12")
+                                .foregroundStyle(Color.white)
+                                .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
+                        }
+                        .frame(height: 100)
+                        
+                    }
+                  
                     NavigationLink {
                         Text("Classes")
                     } label: {
                         Text("Continue")
                             .foregroundStyle(Color.white)
-                            .font(.custom("EuphemiaUCAS-Bold", size: 35.0, relativeTo: .largeTitle))
-                            
-                        
+                            .font(.custom("EuphemiaUCAS-Bold", size: 30.0, relativeTo: .largeTitle))
+                            .padding()
+                            .padding(.vertical, 10)
+                            .background {
+                                RoundedRectangle(cornerRadius: 20.0)
+                                    .foregroundStyle(.gray)
+                                
+                                
+                            }
                     }
                 }
             }
         }
+        
+        
     }
-    
-    }
-    #Preview {
-        ClassSelectionView()
-    }
+}
+#Preview {
+    ClassSelectionView()
+}
+
